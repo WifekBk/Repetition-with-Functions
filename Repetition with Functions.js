43eb4d50c90795577fb9cd246f2a257b2270c756
Wifek Benkhoud <wifekbenkhoud@gmail.com>
2023-01-11 21:16:59 +0100

    5

--- Repetition with Functions.js
+++ Repetition with Functions.js
@@ -71,6 +71,57 @@ function add(number1, number2){
     return inc(number1) + dec(number2)  
   }
 
+  //2 other one
+  function inc(x){
+    return x + 1
+}
+function dec(x){
+    return x - 1
+}
+function add(number1, number2){
+    return inc(number1) + dec(number2)
+  }
+
   //3
+  function isEven(number) {
+    if (number === 0){
+        return true
+    }
+     if (number===1){
+         return false
+     }
+    return isEven(number-2)
+ }  
+
+ //4
+ function range(start, end) {
+   
+    if (start===end-2){
+        return (end-1) + ''
+    }
   
+    return (start+1) + ',' + range(start+1, end)
+}  
+
+function range(start, end) {
+    var x = []
+     if (start===end-1){
+         return x ;
+     }
+    x.push(start+1)  ;
+     return x= x.concat(range(start+1,end)) 
+ }
+
+ //5
+ function multiply(number1, number2) {
+    if (number2===0){
+        return 0
+    }
+     return number1 + multiply(number1, number2-1)
+ } 
+
+ //Advanced
+ //1
+
+
 
